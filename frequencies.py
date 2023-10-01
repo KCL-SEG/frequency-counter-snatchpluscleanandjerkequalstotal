96bc69b73c04b81be8ee3b815dc97031a179455d
snatchpluscleanandjerkequalstotal <k22043969@kcl.ac.uk>
2023-10-01 19:50:54 +0100

    comments to be made in next commit

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
+import itertools
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    groups = []
+    for k,g in itertools.groupby(sorted([str(x) for x in items])):
+        groups.append(list(g))
+    keys = [k for k, g in itertools.groupby(sorted([str(x) for x in items]))]
+    frequencies = {x[0]: len(x[1]) for x in zip(keys, groups)}
+    # note that frequencies = {x[0]: len(x) for x in groups works just the same}
     return frequencies
